146202f2af85a3afd9cfe2340a4c686b2532c798
Darina Maier <darina.maier@epfedu.fr>
2026-03-17 17:03:04 +0100

    SUITE

--- src/js/niveau1.js
+++ src/js/niveau1.js
@@ -1,8 +1,9 @@
 var ast;
 var en1;
+var boutoncourir; 
 
 export default class niveau1 extends Phaser.Scene {
-  
+
   // constructeur de la classe
   constructor() {
     super({
@@ -11,16 +12,10 @@ export default class niveau1 extends Phaser.Scene {
   }
   preload() {
     this.load.image("bg", "src/assets/tuilesn1/1/1_game_background.png");
-    this.load.image("t1", "src/assets/tuilesn1/1/1.png");
-    this.load.image("t2", "src/assets/tuilesn1/1/2.png");
-    this.load.image("t3", "src/assets/tuilesn1/1/3.png");
-    this.load.image("t4", "src/assets/tuilesn1/1/4.png");
-    this.load.image("t5", "src/assets/tuilesn1/1/5.png");
-    this.load.image("t6", "src/assets/tuilesn1/1/6.png");
     this.load.image("plat", "src/assets/tuilesn1/plateform1.png");
     this.load.spritesheet('space_ast', 'src/assets/space_astroids.png', {
-        frameWidth: 256,
-        frameHeight: 256
+      frameWidth: 256,
+      frameHeight: 256
     });
 
     this.load.image("e1", "src/assets/elemn1/en1.png");
@@ -31,33 +26,31 @@ export default class niveau1 extends Phaser.Scene {
     this.load.image("e6", "src/assets/elemn1/en6.png");
 
 
-// chargement de la carte
-  this.load.tilemapTiledJSON("carte", "src/assets/planeterouge.json");  
+    // chargement de la carte
+    this.load.tilemapTiledJSON("carte", "src/assets/planeterouge.json");
   }
 
   create() {
-  const carteDuNiveau = this.add.tilemap("carte");
+    const carteDuNiveau = this.add.tilemap("carte");
 
-// chargement du jeu de tuiles
-    const ts_bg   = carteDuNiveau.addTilesetImage("1_game_background", "bg");
-    const ts_t1   = carteDuNiveau.addTilesetImage("1", "t1");
-    const ts_t2   = carteDuNiveau.addTilesetImage("2", "t2");
-    const ts_t3   = carteDuNiveau.addTilesetImage("3", "t3");
-    const ts_t4   = carteDuNiveau.addTilesetImage("4", "t4");
-    const ts_t5   = carteDuNiveau.addTilesetImage("5", "t5");
-    const ts_t6   = carteDuNiveau.addTilesetImage("6", "t6");
+    // chargement du jeu de tuiles
+    const ts_bg = carteDuNiveau.addTilesetImage("1_game_background", "bg");
+    
     const ts_plat = carteDuNiveau.addTilesetImage("plateform1", "plat");
-         
-  const tilesets = [ts_bg, ts_t1, ts_t2, ts_t3, ts_t4, ts_t5, ts_t6, ts_plat];
 
-    const calque_background  = carteDuNiveau.createLayer("calque_planète_rouge",  tilesets);
-    const calque_plateformes = carteDuNiveau.createLayer("calque_platform_rouge", tilesets);
+    const tilesets = [ts_bg, ts_plat];
 
-    // Collision sur les tuiles solides
+    const calque_background  = carteDuNiveau.createLayer("calque_planète_rouge", tilesets);
+    const calque_plateformes = carteDuNiveau.createLayer("calque_platform",      tilesets);
+    const calque_death       = carteDuNiveau.createLayer("calque_platform_death", tilesets);
+
+
+    // Collision sur les plateformes
     calque_plateformes.setCollisionByProperty({ estSolide: true });
-    calque_plateformes.setCollisionByProperty({ estsolide: true });
+    // Collision death — tue le joueur 
+    calque_death.setCollisionByProperty({ estSolide: true });
 
-    this.player = this.physics.add.sprite(100, 450, 'astronaut');
+    this.player = this.physics.add.sprite(120, 450, 'astronaut');
     this.player.setSize(50, 70);
     this.player.setOffset(36, 10);
     this.player.setCollideWorldBounds(true);
@@ -66,64 +59,88 @@ export default class niveau1 extends Phaser.Scene {
     ast = this.physics.add.sprite(2048, 300, 'space_ast');
 
     this.clavier = this.input.keyboard.createCursorKeys();
-    this.physics.add.collider(this.player, calque_plateformes);
-    this.cameras.main.setBounds(0, 0, 3072, 768); 
+    this.cameras.main.setBounds(0, 0, 3072, 768);
     this.cameras.main.startFollow(this.player);
     this.physics.world.setBounds(0, 0, 3072, 768); // ← même dimensions que la caméra
-    // gravité
+
+    // bouton définit 
     this.toucheGravite = this.input.keyboard.addKey('G');
+    boutoncourir = this.input.keyboard.addKey('C');
 
     //mise en place en1
-     var groupe_en1 = this.physics.add.group(); 
+    var groupe_en1 = this.physics.add.group();
 
-     var en1Images = ["e1", "e2", "e3", "e4", "e5", "e6"]
+    var en1Images = ["e1", "e2", "e3", "e4", "e5", "e6"]
     for (let i = 0; i < 6; i++) {
-      var coordX = 300 + 550 * i;
+      var coordX = 150 + 600 * i;
       var coordY = 0;
       groupe_en1.create(coordX, coordY, en1Images[i]);
     };
-    this.physics.add.collider(groupe_en1, calque_plateformes); 
+    this.physics.add.collider(this.player, calque_plateformes);
+    this.physics.add.collider(this.player, calque_death, () => {
+      this.scene.restart(); // redémarre la scène actuelle
+    });
+    this.physics.add.collider(groupe_en1, calque_plateformes);
     this.physics.add.overlap(this.player, groupe_en1, ramasseren1, null, this);
-  this.gravityInverted == false;
+    this.gravityInverted == false;
   }
 
   update() {
- if (this.clavier.right.isDown) {
-      this.player.setVelocityX(160); 
-      this.player.direction='droite'
-      this.player.anims.play('anim_droite', true);
-    } else if (this.clavier.left.isDown) {
-      this.player.setVelocityX(-160); 
-      this.player.direction='gauche'
-      this.player.anims.play('anim_gauche', true);
-    } else{
-      this.player.setVelocityX(0); // ← action séparée
-    if (this.player.direction == 'droite') {
-        this.player.anims.play('immobiledroit', true);
+    if (boutoncourir.isDown) {
+      if (this.player.direction == 'droite') {
+      //  this.player.setOffset(76, 0);
+
+        this.player.setVelocityX(300); // plus rapide en courant
+        this.player.anims.play('courirdroite', true);
+      } else {
+      //    this.player.setOffset(36, 10);
+        this.player.setVelocityX(-300); // plus rapide en courant
+        this.player.anims.play('courirgauche', true); //gauche plus tard
+      }
     } else {
-        this.player.anims.play('immobilegauche', true);
+      if (this.clavier.right.isDown) {
+        this.player.setVelocityX(160);
+        this.player.direction = 'droite'
+        this.player.anims.play('anim_droite', true);
+        this.player.setOffset(36, 10);
+
+      } else if (this.clavier.left.isDown) {
+        this.player.setVelocityX(-160);
+        this.player.direction = 'gauche'
+        this.player.anims.play('anim_gauche', true);
+        this.player.setOffset(42, 10);
+      } else {
+        this.player.setVelocityX(0); // ← action séparée
+        if (this.player.direction == 'droite') {
+          this.player.anims.play('immobiledroit', true);
+        } else {
+          this.player.anims.play('immobilegauche', true);
+        }
+      }
+
+      if (this.clavier.space.isDown && this.player.body.blocked.down) {
+        this.player.setVelocityY(-300);
+      }
+
+      if (Phaser.Input.Keyboard.JustDown(this.clavier.up)) {
+        this.scene.start('pageprincipale');
+      }
+
+      // appuie sur G pour changer la gravité 
+      if (Phaser.Input.Keyboard.JustDown(this.toucheGravite)) {
+        if (this.gravityInverted == true) {
+          this.player.body.gravity.y = -600;  // gravité vers le bas
+          this.gravityInverted = false;
+          //    this.physics.world.gravity.y = -200; // gravité vers le haut
+          this.player.setFlipY(true); // retourne le sprite du joueur
+        } else {
+          this.gravityInverted = true;
+          this.player.body.gravity.y = 0;  // gravité vers le bas
+          this.player.setFlipY(false); // remet le sprite du joueur à l'endroit
+        }
       }
     }
-    if (this.clavier.space.isDown && this.player.body.blocked.down) {
-        this.player.setVelocityY(-300);
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.clavier.up)) {
-        this.scene.start('pageprincipale');
-}
- // appuie sur G pour changer la gravité 
-if (Phaser.Input.Keyboard.JustDown(this.toucheGravite)) {
-    if (this.gravityInverted == true) {
-            this.player.body.gravity.y = -600;  // gravité vers le bas
-this.gravityInverted = false;
-//    this.physics.world.gravity.y = -200; // gravité vers le haut
-    this.player.setFlipY(true); // retourne le sprite du joueur
-      } else {
-      this.gravityInverted = true;
-      this.player.body.gravity.y = 0;  // gravité vers le bas
-      this.player.setFlipY(false); // remet le sprite du joueur à l'endroit
-}
   }
-}
 }
 
 //autres fonctions 
@@ -131,3 +148,4 @@ this.gravityInverted = false;
 function ramasseren1(personnage, element) {
   element.disableBody(true, true);
 }
+
--- src/js/pageprincipale.js
+++ src/js/pageprincipale.js
@@ -7,187 +7,209 @@ var player;
 var clavier;
 var p1;
 var boutonentrer;
+var boutoncourir;
 
 // définition de la classe "selection"
 export default class pageprincipale extends Phaser.Scene {
-  constructor() {
-    super({ key: "pageprincipale" }); // mettre le meme nom que le nom de la classe
-  }
-
-  /***********************************************************************/
-  /** FONCTION PRELOAD 
-/***********************************************************************/
-
-  preload() {
-    // fond étoiles 
-    this.load.image("backgp", "src/assets/backgp.jpg");
-//PLANETES 
-    this.load.image('planete1', 'src/assets/planet1.png'); 
-    this.load.image('planete2', 'src/assets/planet2.png'); 
-    this.load.image('planete3', 'src/assets/planet4.png'); 
-    this.load.image('planete4', 'src/assets/planet5.png'); 
-    this.load.image('planete5', 'src/assets/planet7.png'); 
-    this.load.image('planete6', 'src/assets/planet10.png'); 
-    this.load.image('planete7', 'src/assets/planet19.png');
-    this.load.image('planete8', 'src/assets/planet15.png');
-    this.load.image('background', 'src/assets/etoiles.png');
-    this.load.image('background2', 'src/assets/etoilebleue.png');
-    this.load.image('plateforme1', 'src/assets/plateforme1.png')
-
-    this.load.spritesheet('astronaut', 'src/assets/astronaut.png', {
-        frameWidth: 130,
-        frameHeight: 90
-    });
-    this.load.spritesheet('astronautinverse', 'src/assets/astronautinverse.png', {
-        frameWidth: 130,
-        frameHeight: 90
-    });
-  }
-
-  /***********************************************************************/
-  /** FONCTION CREATE 
-/***********************************************************************/
-
-  create() {
-// fond 
-    let background = this.add.image(400, 300, 'backgp');
-    background.setScrollFactor(0.4); // le fond ne bouge pas avec la caméra
-
-//planetes
-    p1 = this.physics.add.staticSprite(224,  384, 'planete1');
-    var p2 = this.physics.add.staticSprite(470,  384, 'planete2');
-    var p3 = this.physics.add.staticSprite(716,  384, 'planete3');
-    var p4 = this.physics.add.staticSprite(962,  384, 'planete4');
-    var p5 = this.physics.add.staticSprite(1208, 384, 'planete5');
-    var p6 = this.physics.add.staticSprite(1454, 384, 'planete6');
-    var p7 = this.physics.add.staticSprite(1700, 384, 'planete7');
-    var p8 = this.physics.add.staticSprite(1946, 384, 'planete8');
-    var plateforme = this.physics.add.staticSprite(0,  384, 'plateforme1');
-
-//définitions des hitbox circulaires pour planètes 
-    p1.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
-    p1.body.setOffset(56, 56); // décale la hitbox de 64 pixels vers la gauche et 64 pixels vers le haut pour la centrer sur le sprite
-    
-    p2.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
-    p2.body.setOffset(59, 62);
-
-    p3.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
-    p3.body.setOffset(56, 56);
-
-    p4.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
-    p4.body.setOffset(56, 56);
-
-    p5.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
-    p5.body.setOffset(56, 56);
-
-    p6.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
-    p6.body.setOffset(56, 56);
-
-    p7.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
-    p7.body.setOffset(56, 56);
-
-    p8.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
-    p8.body.setOffset(56, 56);
-
-//création joueur 
-    player = this.physics.add.sprite(0, 10, 'astronaut'); 
-    player.setSize(65, 45); 
-    player.direction='droite'
-
-//parametres joueur 
-    player.setCollideWorldBounds(true);
-    this.physics.world.setBounds(0, 0, 2048, 768);
-    this.cameras.main.setBounds(0, 0, 2048, 768);
-    this.cameras.main.startFollow(player);
-
-//collisions 
-    clavier = this.input.keyboard.createCursorKeys();
-    this.physics.add.collider(player, plateforme);
-    this.physics.add.collider(player, p1);
-    this.physics.add.collider(player, p2);
-    this.physics.add.collider(player, p3);
-    this.physics.add.collider(player, p4);
-    this.physics.add.collider(player, p5);
-    this.physics.add.collider(player, p6);
-    this.physics.add.collider(player, p7);
-    this.physics.add.collider(player, p8);
-    player.setVelocityX(0); // vitesse horizontale constante vers la droite
-    
-    //entrer niveau 
-    boutonentrer = this.input.keyboard.addKey('E');
-
-    //animations
-    this.anims.create({
-        key: 'anim_droite',
-        frames: this.anims.generateFrameNumbers('astronaut', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-    this.anims.create({
-        key: 'anim_gauche',
-        frames: this.anims.generateFrameNumbers('astronautinverse', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-    this.anims.create({
-        key: 'immobiledroit',
-        frames: [ { key: 'astronaut', frame: 21 } ],
-        frameRate: 1
-    });
-    this.anims.create({
-        key: 'immobilegauche',
-        frames: [ { key: 'astronautinverse', frame: 21 } ],
-        frameRate: 1
-    });
-    this.anims.create({
-        key: 'sautdroit',
-        frames: this.anims.generateFrameNumbers('astronaut', { start: 30, end: 35 }),
-        frameRate: 10,
-        repeat: -1
-    })
-    this.anims.create({
-        key: 'courir',
-        frames: this.anims.generateFrameNumbers('astronaut', { start: 12, end: 19 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-  }
-
-  /***********************************************************************/
-  /** FONCTION UPDATE 
-/***********************************************************************/
-
-  update() {
-    if (clavier.right.isDown) {
-      player.setVelocityX(160); 
-      player.direction='droite'
-      player.anims.play('anim_droite', true);
-    } else if (clavier.left.isDown) {
-      player.setVelocityX(-160); 
-      player.direction='gauche'
-      player.anims.play('anim_gauche', true);
-    } else{
-       player.setVelocityX(0); // ← action séparée
-    if (player.direction == 'droite') {
-        player.anims.play('immobiledroit', true);
-    } else {
-        player.anims.play('immobilegauche', true);
-      }
+    constructor() {
+        super({ key: "pageprincipale" }); // mettre le meme nom que le nom de la classe
     }
-    if (clavier.space.isDown && player.body.blocked.down) {
-        player.setVelocityY(-300);
+
+    /***********************************************************************/
+    /** FONCTION PRELOAD 
+  /***********************************************************************/
+
+    preload() {
+        // fond étoiles 
+        this.load.image("backgp", "src/assets/backgp.jpg");
+        //PLANETES 
+        this.load.image('planete1', 'src/assets/planet1.png');
+        this.load.image('planete2', 'src/assets/planet2.png');
+        this.load.image('planete3', 'src/assets/planet4.png');
+        this.load.image('planete4', 'src/assets/planet5.png');
+        this.load.image('planete5', 'src/assets/planet7.png');
+        this.load.image('planete6', 'src/assets/planet10.png');
+        this.load.image('planete7', 'src/assets/planet19.png');
+        this.load.image('planete8', 'src/assets/planet15.png');
+        this.load.image('background', 'src/assets/etoiles.png');
+        this.load.image('background2', 'src/assets/etoilebleue.png');
+        this.load.image('plateforme1', 'src/assets/plateforme1.png')
+
+        this.load.spritesheet('astronaut', 'src/assets/astronaut.png', {
+            frameWidth: 130,
+            frameHeight: 90
+        });
+        this.load.spritesheet('astronautinverse', 'src/assets/astronautinverse.png', {
+            frameWidth: 130,
+            frameHeight: 90
+        });
     }
-    if (Phaser.Input.Keyboard.JustDown(boutonentrer)) {
-    if (this.physics.overlap(player, p1)) {
-        this.scene.start('niveau1');
+
+    /***********************************************************************/
+    /** FONCTION CREATE 
+  /***********************************************************************/
+
+    create() {
+        // fond 
+        let background = this.add.image(400, 300, 'backgp');
+        background.setScrollFactor(0.4); // le fond ne bouge pas avec la caméra
+
+        //planetes
+        p1 = this.physics.add.staticSprite(224, 384, 'planete1');
+        var p2 = this.physics.add.staticSprite(470, 384, 'planete2');
+        var p3 = this.physics.add.staticSprite(716, 384, 'planete3');
+        var p4 = this.physics.add.staticSprite(962, 384, 'planete4');
+        var p5 = this.physics.add.staticSprite(1208, 384, 'planete5');
+        var p6 = this.physics.add.staticSprite(1454, 384, 'planete6');
+        var p7 = this.physics.add.staticSprite(1700, 384, 'planete7');
+        var p8 = this.physics.add.staticSprite(1946, 384, 'planete8');
+        var plateforme = this.physics.add.staticSprite(0, 384, 'plateforme1');
+
+        //définitions des hitbox circulaires pour planètes 
+        p1.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
+        p1.body.setOffset(56, 56); // décale la hitbox de 64 pixels vers la gauche et 64 pixels vers le haut pour la centrer sur le sprite
+
+        p2.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
+        p2.body.setOffset(59, 62);
+
+        p3.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
+        p3.body.setOffset(56, 56);
+
+        p4.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
+        p4.body.setOffset(56, 56);
+
+        p5.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
+        p5.body.setOffset(56, 56);
+
+        p6.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
+        p6.body.setOffset(56, 56);
+
+        p7.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
+        p7.body.setOffset(56, 56);
+
+        p8.body.setCircle(94); // définit une hitbox circulaire de rayon 128 pixels centrée sur le sprite
+        p8.body.setOffset(56, 56);
+
+        //création joueur 
+        player = this.physics.add.sprite(0, 10, 'astronaut');
+        player.setSize(65, 45);
+        player.direction = 'droite'
+
+        //parametres joueur 
+        player.setCollideWorldBounds(true);
+        this.physics.world.setBounds(0, 0, 2048, 768);
+        this.cameras.main.setBounds(0, 0, 2048, 768);
+        this.cameras.main.startFollow(player);
+
+        //collisions 
+        clavier = this.input.keyboard.createCursorKeys();
+        this.physics.add.collider(player, plateforme);
+        this.physics.add.collider(player, p1);
+        this.physics.add.collider(player, p2);
+        this.physics.add.collider(player, p3);
+        this.physics.add.collider(player, p4);
+        this.physics.add.collider(player, p5);
+        this.physics.add.collider(player, p6);
+        this.physics.add.collider(player, p7);
+        this.physics.add.collider(player, p8);
+        player.setVelocityX(0); // vitesse horizontale constante vers la droite
+
+        //entrer niveau 
+        boutonentrer = this.input.keyboard.addKey('E');
+        boutoncourir = this.input.keyboard.addKey('C');
+
+        //animations
+        this.anims.create({
+            key: 'anim_droite',
+            frames: this.anims.generateFrameNumbers('astronaut', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'anim_gauche',
+            frames: this.anims.generateFrameNumbers('astronautinverse', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'immobiledroit',
+            frames: [{ key: 'astronaut', frame: 21 }],
+            frameRate: 1
+        });
+        this.anims.create({
+            key: 'immobilegauche',
+            frames: [{ key: 'astronautinverse', frame: 21 }],
+            frameRate: 1
+        });
+        this.anims.create({
+            key: 'sautdroit',
+            frames: this.anims.generateFrameNumbers('astronaut', { start: 30, end: 35 }),
+            frameRate: 10,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'courirdroite',
+            frames: this.anims.generateFrameNumbers('astronaut', { start: 12, end: 17 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'courirgauche',
+            frames: this.anims.generateFrameNumbers('astronautinverse', { start: 12, end: 17 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
     }
-     if (Phaser.Input.Keyboard.JustDown(boutoncourir)) {
-        player.anims.play('courir', true);
-  }
+
+    /***********************************************************************/
+    /** FONCTION UPDATE 
+  /***********************************************************************/
+
+    update() {
+        if (boutoncourir.isDown) {
+            if (player.direction == 'droite') {
+                player.setVelocityX(300); // plus rapide en courant
+                player.anims.play('courirdroite', true);
+            }
+            else {
+                player.setVelocityX(-300); // plus rapide en courant
+                player.anims.play('courirgauche', true); //gauche plus tard
+            }
+        } else {
+            if (clavier.right.isDown) {
+                player.setVelocityX(160);
+                player.direction = 'droite'
+                player.anims.play('anim_droite', true);
+            } else if (clavier.left.isDown) {
+                player.setVelocityX(-160);
+                player.direction = 'gauche'
+                player.anims.play('anim_gauche', true);
+            } else {
+                player.setVelocityX(0); // ← action séparée
+                if (player.direction == 'droite') {
+                    player.anims.play('immobiledroit', true);
+                } else {
+                    player.anims.play('immobilegauche', true);
+                }
+            }
+        }
+
+        if (clavier.space.isDown && player.body.blocked.down) {
+            player.setVelocityY(-300);
+        }
+
+
+        if (Phaser.Input.Keyboard.JustDown(boutonentrer)) {
+            if (this.physics.overlap(player, p1)) {
+                this.scene.start('niveau1');
+            }
+        }
+
+
+    }
 }
-}
-}
+
 /***********************************************************************/
 /** CONFIGURATION GLOBALE DU JEU ET LANCEMENT 
 /***********************************************************************/
