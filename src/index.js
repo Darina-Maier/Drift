018ca16ab4c5f83ad448a41a589e98aa20099e2e
Charlotte <c.tournamille0@gmail.com>
2026-03-18 22:48:04 +0100

    teleport n7

--- src/index.js
+++ src/index.js
@@ -30,9 +30,9 @@ var config = {
       debug: true // permet de voir les hitbox et les vecteurs d'acceleration quand mis à true
     }
   },
-  scene: [niveau7, intro,pageprincipale, niveau1, niveau2, niveau3, niveau4, niveau5, acceuil, niveau6]
+  scene: [acceuil, intro,pageprincipale, niveau1, niveau2, niveau3, niveau4, niveau5, niveau6, niveau7]
 };
 
 // création et lancement du jeu
 var game = new Phaser.Game(config);
-game.scene.start("acceuil"); // démarre la scène pageprincipale
+game.scene.start("pageprincipale"); // démarre la scène pageprincipale
